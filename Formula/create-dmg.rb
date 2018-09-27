401e0b0145c8358bc922a92ae31e2c70551f50e0
Martin Delille <martin@phonations.com>
2018-09-27 15:08:19 +0200

    brew create https://github.com/andreyvit/create-dmg/releases/tag/v1.0.0.3

---
+++ Formula/create-dmg.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class CreateDmg < Formula
+  desc "A shell script to build fancy DMGs"
+  homepage ""
+  url "https://github.com/andreyvit/create-dmg/releases/tag/v1.0.0.3"
+  sha256 "4557de44fca40e22c0d44b89fcb722d1dc6e6df73049c7756675d4793bf1c9e2"
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test create-dmg`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
