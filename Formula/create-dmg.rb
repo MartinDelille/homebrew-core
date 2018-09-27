ca729ba2922a0e58c70e2c88f50dcea1ede46f19
Martin Delille <martin@phonations.com>
2018-09-27 15:42:38 +0200

    create-dmg better installation
    Removing comment
    add test

--- Formula/create-dmg.rb
+++ Formula/create-dmg.rb
@@ -1,34 +1,26 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class CreateDmg < Formula
   desc "A shell script to build fancy DMGs"
-  homepage ""
+  homepage "https://github.com/andreyvit/create-dmg"
   url "https://github.com/andreyvit/create-dmg/releases/tag/v1.0.0.3"
   sha256 "4557de44fca40e22c0d44b89fcb722d1dc6e6df73049c7756675d4793bf1c9e2"
-  # depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    bin.install "create-dmg"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test create-dmg`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "#{bin}/create-dmg",
+      "--volname \"Application Installer\"",
+      "--volicon \"application\_icon.icns\"",
+      "--background \"installer\_background.png\"",
+      "--window-pos 200 120",
+      "--window-size 800 400",
+      "--icon-size 100",
+      "--icon Application.app 200 190",
+      "--hide-extension Application.app",
+      "--app-drop-link 600 185",
+      "Application-Installer.dmg",
+      "."
+    system "open", "Application-Installer.dmg"
   end
 end
